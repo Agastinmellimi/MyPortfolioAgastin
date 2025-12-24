01d827548f5fd3c3dd6f9eb88267ce026eef4198
Agastinmellimi <agastinmellimi2001@gmail.com>
2025-12-24 16:31:09 +0530

    remove contact us

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import Header from './Components/Header'
-import Home from './Components/Home'
-import Education from './Components/Education'
-import Skills from './Components/Skills';
-import Projects from './Components/Projects';
-import About from './Components/About'
-import Footer from './Components/Footer';
-import ContactUs from './Components/ContactUs';
-import './App.css';
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Education from "./Components/Education";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import "./App.css";
 
 const App = () => (
   <>
-  <Header/>
-  <Home/>
-  <Education/>
-  <Skills/>
-  <Projects/>
-  <About/>
-  <ContactUs/>
-  <Footer/>
+    <Header />
+    <Home />
+    <Education />
+    <Skills />
+    <Projects />
+    <About />
+    <Footer />
   </>
-)
+);
 
 export default App;
